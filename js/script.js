d9a9014a76a77cfdc422a4a4bb9601ebc4965c3d
Timmyfirst <timmyc71@gmail.com>
2020-07-07 22:56:38 +0200

    First version

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ let generator = {};
  * @param {}  should be define later.
  * @return {} should be define later.
  */
-generator.Generate = function(){
+generator.main = function(){
 	var i = document.getElementById('nb_car').value;
 	if (generator.VerifyWholeNatural(i)){
 		if (parseInt(i) <= 5) {
@@ -14,53 +14,49 @@ generator.Generate = function(){
 		}
 		else {
 
-			var result="";
-
-			var caracteres = [];
-			for (compt = 65; compt < 91; compt++) {
-	  			caracteres.push(compt);
-			}
-			for (compt = 97; compt < 123; compt++) {
-	  			caracteres.push(compt);
-			}
-
-			if (document.getElementById("chiffres").checked == true)
-				var b_chi = true;
-			else
-				var b_chi = false;
-
-			if (document.getElementById("carnas").checked == true)
-				var b_crn = true;
-			else
-				var b_crn = false;
-
-			if (b_chi == true) {
-				for (compt = 48; compt < 58; compt++) {
-	  				caracteres.push(compt);
-				}
-			}
-
-			if (b_crn == true) {
-				for (compt = 33; compt < 46; compt++) {
-	  				caracteres.push(compt);
-				}
-				for (compt = 58; compt < 65; compt++) {
-	  				caracteres.push(compt);
-				}
-				for (compt = 91; compt < 97; compt++) {
-	  				caracteres.push(compt);
-				}
-			}
-
-			for (compt = 0; compt <= parseInt(i); compt++) {
-				result += String.fromCharCode(caracteres[parseInt(Math.random() * (caracteres.length - 0) + 0)]);
-	  	}
+			result = generator.Generate(i, document.getElementById("chiffres").checked == true, document.getElementById("carnas").checked == true)
 
 	  	document.getElementById('result').value = result;
 		}
 	} else {
 		alert('Veuillez renseigner un chiffre ou un nombre supérieur à 5.');
 	}
+}
+
+generator.Generate = function(nb_car, b_chi, b_crn){
+	var result="";
+
+	var caracteres = [];
+	for (compt = 65; compt < 91; compt++) {
+			caracteres.push(compt);
+	}
+	for (compt = 97; compt < 123; compt++) {
+			caracteres.push(compt);
+	}
+
+	if (b_chi == true) {
+		for (compt = 48; compt < 58; compt++) {
+				caracteres.push(compt);
+		}
+	}
+
+	if (b_crn == true) {
+		for (compt = 33; compt < 46; compt++) {
+				caracteres.push(compt);
+		}
+		for (compt = 58; compt < 65; compt++) {
+				caracteres.push(compt);
+		}
+		for (compt = 91; compt < 97; compt++) {
+				caracteres.push(compt);
+		}
+	}
+
+	for (compt = 0; compt < parseInt(nb_car); compt++) {
+		result += String.fromCharCode(caracteres[parseInt(Math.random() * (caracteres.length - 0) + 0)]);
+	}
+
+	return result
 }
 
 /**
